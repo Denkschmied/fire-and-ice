cdd96814503074205ea6b5149bdf202ef3feb5c4
Fabio Aufinger <aufinger.fabio@gmail.com>
2019-05-28 10:29:54 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,35 @@
+#FireHead Config-Datei
+#18.05.2019
+#by Fabio Aufinger
+
+
+#Track
+XLimits = (-5, 5)
+YLimits = (0, 10)
+steps = 0.1
+
+#Motoren-Richtung
+motorL_forward = 1
+motorL_backward = 2
+motorR_forward = 3
+motorR_backward = 4
+
+#Motoren-PWM
+motorL_PWM = 5
+motorR_PWM = 6
+
+#Kamera
+
+
+#Ultraschall
+Trigger = 7
+Echo = 8
+
+#Flammsensor
+flame_input = 9
+
+#Drehzahlsensoren
+rpmL = 10
+rpmR = 11
+
+#Gyro
