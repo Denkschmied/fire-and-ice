1b5ee008b0b5d1a1e9831d80c7b7cd73465a026a
amp enator <amp1enator@gmail.com>
2019-06-03 22:19:00 +0200

    Hexabot (Matteo) Code

---
+++ Hexabot/hexabot.py
@@ -0,0 +1,37 @@
+### Start/Stop
+###
+###
+###
+###
+###
+
+
+### Startposition ###
+X = 0
+Y = 0
+dirX = 0
+
+### Start ###
+#Press s to Start
+print("s to start, e to exit, r to return (only after start)")
+
+try:
+ 	while True: # create an infinte loop to keep the script running
+ 	 	time.sleep(0.1)
+
+while(1):
+    x = raw_input()
+    if x == 's':
+
+        distL =
+
+    if x == 'r':
+        if
+            else
+
+except KeyboardInterrupt:
+    pass
+
+finally:
+    c.stop()
+    GPIO.cleanup()
