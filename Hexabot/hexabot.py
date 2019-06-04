1181c239d4d55ae064b7aa85745be781d4cb3ab2
amp enator <amp1enator@gmail.com>
2019-06-05 00:01:46 +0200

    -

--- Hexabot/hexabot.py
+++ Hexabot/hexabot.py
@@ -4,14 +4,11 @@
 ###
 ###
 ###
+import time
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
+GPIO.setwarnings(True)
 import config as cf
-import drive as d
-import sonic as s
-#import gyro as g
-
 #### GPIO setup PINs ###
 # Motor
 GPIO.setup(cf.motorL_PWM, GPIO.OUT)
@@ -21,6 +18,10 @@ GPIO.setup(cf.motorL_backward, GPIO.OUT)
 GPIO.setup(cf.motorR_PWM, GPIO.OUT)
 GPIO.setup(cf.motorR_forward, GPIO.OUT)
 GPIO.setup(cf.motorR_backward, GPIO.OUT)
+
+import drive as d
+import sonic as s
+#import gyro as g
 
 # Ultraschall
 GPIO.setup(cf.TriggerL, GPIO.OUT)
@@ -36,24 +37,38 @@ GPIO.setup(cf.rpmR, GPIO.IN)
 
 # FLAME
 GPIO.setup(cf.flame_input, GPIO.IN)
-print("s to start, e to exit, r to return (only after start)")
+
+
+# print("s to start, e to exit, r to return (only after start)")
+# while True:
+#     # Press s to Start
+#     #try:
+#     x = raw_input()
+#     if x == 's':
+#         d.drive("L", "F", 10)
+#         d.drive("R", "F", 10)
+#         print(s.readDistance(cf.EchoL, cf.TriggerL))
+#     if x == 'r':
+#         d.stop("L")
+#         d.stop("R")
+#         GPIO.cleanup()
+#     # except KeyboardInterrupt:
+#     #     c.stop()
+#     #     GPIO.cleanup()
+#     #     raise Exception, "Ende"
 while True:
-    # Press s to Start
-    #try:
-    x = raw_input()
-    if x == 's':
-        d.drive("L", "F", 10)
-        d.drive("R", "F", 10)
-        print(s.readDistance(cf.EchoL, cf.TriggerL))
-    if x == 'r':
-        d.stop("L")
-        d.stop("R")
+    d.drive("L", "F", 20)
+    # speed = speed + 0.1
+    # print(speed)
+    print("Sonic Links:", s.readDistance(cf.EchoL, cf.TriggerL))
+    print("Sonic Mitte:", s.readDistance(cf.EchoM, cf.TriggerM))
+    print("Sonic Rechts:", s.readDistance(cf.EchoR, cf.TriggerR))
 
-    # except KeyboardInterrupt:
-    #     c.stop()
-    #     GPIO.cleanup()
-    #     raise Exception, "Ende"
 
+# finally:
+#     d.stop("L")
+#     d.stop("R")
+#     GPIO.cleanup()
 #
 # ### Startposition ###
 # X = 0
